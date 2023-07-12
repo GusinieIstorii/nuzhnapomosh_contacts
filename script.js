3b71cc054e325ef74660992558dde5c4d0c19fd5
Kseniia <ksenia.guseva789@gmail.com>
2023-07-12 15:14:43 +0300

    mobile adaptation

--- script.js
+++ script.js
@@ -60,15 +60,15 @@ function toggleModal() {
     modal.classList.toggle("show-modal");
 }
 
-// function windowOnClick(event) {
-//     if (event.target === modal) {
-//         toggleModal();
-//     }
-// }
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
 
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
-// window.addEventListener("click", windowOnClick);
+window.addEventListener("click", windowOnClick);
 
 // form email input validation
 const email = document.getElementById("email");
